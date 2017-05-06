b287e1772575a4fe74b6b448e8d4df5001583dee
CristianDP <CristianDP@Cristian>
2017-05-05 22:04:58 -0500

    Factura JPQL

--- src/main/java/co/edu/ims/servicio/res/FacturaRest.java
+++ src/main/java/co/edu/ims/servicio/res/FacturaRest.java
@@ -1,9 +1,11 @@
 package co.edu.ims.servicio.res;
 
 import co.ims.soa.sswcompraventa.modelo.Factura;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -23,6 +25,16 @@ public class FacturaRest {
     @Produces("application/json")
     public Factura buscar(@PathParam("id") Integer pId){
         return em.find(Factura.class, pId);    
+    }
+    
+    @GET    
+    @Produces("application/json")
+    public List<Factura> buscar(){
+        System.out.println("init...");
+        String jpql = "SELECT fac FROM Factura fac";
+        TypedQuery <Factura> f = em.createQuery(jpql, Factura.class);
+        List<Factura> resultado = f.getResultList();
+        return resultado;    
     }
     
     @PUT

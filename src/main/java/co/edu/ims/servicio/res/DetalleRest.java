acbda9fdd7af4ea0556be08d17fcd3c769def256
Silvia <Silvia@Silvia-PC>
2017-05-19 16:08:56 -0500

    VersiowebApp 3.0

--- src/main/java/co/edu/ims/servicio/res/DetalleRest.java
+++ src/main/java/co/edu/ims/servicio/res/DetalleRest.java
@@ -1,11 +1,14 @@
 package co.edu.ims.servicio.res;
 
 import co.ims.soa.sswcompraventa.modelo.Detalle;
+import co.ims.soa.sswcompraventa.modelo.Producto;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -17,6 +20,9 @@ public class DetalleRest {
 
     @PersistenceContext(unitName = "compraventaPU")
     protected EntityManager em;
+    
+    @Inject SingletonEJB singletonEJB;
+    @Inject ProductoRest productoRest;
 
     @GET
     @Path("{id}")
@@ -33,6 +39,39 @@ public class DetalleRest {
         em.persist(entity);
         em.flush();
         return entity;
+    }
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Detalle actualizar(Detalle p){
+        em.merge(p);  
+        singletonEJB.incrementarCodigo();
+        System.out.println(singletonEJB.getCodigoOperacion());
+        return p;
+    }
+    
+    @PUT
+    @Path("/agregarpelicula/{id}")
+    @Produces("application/json")      
+    @Consumes("application/json")
+    public Detalle agregarProducto(@PathParam("id") Integer pId, Detalle d){       
+        Producto p = productoRest.buscar(pId);
+        d.setIdProducto(p);
+        em.persist(d);
+        em.flush();        
+        return d;
+    }
+    
+    @PUT
+    @Path("/agregarpelicula/{id}")
+    @Produces("application/json")      
+    @Consumes("application/json")
+    public Detalle agregarFactura(@PathParam("id") Integer pId, Detalle d){       
+        Producto p = productoRest.buscar(pId);
+        d.setIdProducto(p);
+        em.persist(d);
+        em.flush();        
+        return d;
     }
 
 }
